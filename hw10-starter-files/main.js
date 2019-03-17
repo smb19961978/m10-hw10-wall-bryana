c720ea1b6fd1331083cd9c3e0ca31424f322eda6
Bryana <bryananwall@gmail.com>
2019-03-17 08:37:51 -0500

    add arrow functions to js file

--- hw10-starter-files/main.js
+++ hw10-starter-files/main.js
@@ -1,1 +1,47 @@
 // It is always helpful to use comments in your code!
+
+
+//boiling temps
+const boilingtemp = document.getElementById("boiling-temp");
+
+const boiling = () => boilingtemp.innerHTML = "212";
+
+boiling();
+
+//freezing temps
+
+const freezingtemp = document.getElementById("freezing-temp");
+
+freezingtemp.innerHTML = "32";
+
+//water temps
+
+const watertemp = document.getElementById("water-temp");
+
+watertemp.innerHTML = "75";
+
+//set time out
+
+const boilingid = document.getElementById("boiling");
+
+const frozenid = document.getElementById("frozen");
+
+const goodid = document.getElementById("good-temp");
+
+setTimeout(function(){
+    
+    if (watertemp > 212){
+    boilingid.classList.remove("hide").add("show");
+    }
+    
+    else if (watertemp< 32){
+        frozenid.classList.remove("hide").add("show");
+    }
+        
+    else {
+        goodid.classList.remove("hide").add("show");
+    }
+    
+
+    
+}, 3000);
